edcac373a8edab08965761b71223969aeabe372f
hkn96 <63206090+hkn96@users.noreply.github.com>
2021-03-22 15:44:39 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<footer class="section">
+    <div class="center grey-text">Copyright 2021 by Hkn</div>
+</footer>
+
+
+</body>
+
+
---
+++ header.php
@@ -0,0 +1,32 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pizza </title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+        .brand{
+            background:  #ff652f !important;
+        }
+        .brand-text{
+            color: black !important;
+        }
+        form{
+            max-width: 460px;
+            margin: 20px auto;
+            padding: 20px;
+        }
+
+    </style>
+</head>
+<body class="grey lighten-4">
+<nav class="grey z-depth-0">
+    <div class="container">
+        <a href="#" class="brand-logo brand-text">Pizza Service</a>
+        <ul id="nav-mobile" class="right hide-on-small-and-down">
+            <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
+        </ul>
+    </div>
+</nav>
